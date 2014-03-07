9cc2311cf8517f30d70ec6771a1779d0b44f04c7
suisho <suisho@suisho-no-MacBook-Air.local>
2014-03-07 22:27:53 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseOnlyMail::Application.config.secret_key_base = 'b72a435f7d19262b41e683ea0e49bcce0cb9883efdf1eafe1dd9890748650554340092d97ab082ef977033c50b6749af2d33b27b0e10a15954f5c940ddb7438f'
